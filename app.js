995adfd20e79c70a830b6f32dfea54f9972c0afe
gnu <gnu@debian7.debian7.com>
2014-05-14 23:27:53 -0500

    Actualizar nombre del repositorio

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var app = express();
 
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'server')));
+app.use(express.static(path.join(__dirname, 'www')));
 
 app.listen(5000);
